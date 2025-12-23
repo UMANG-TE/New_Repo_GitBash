8448027ac6e8098c84b7445c2320352cd0dc06fd
UMANG-TE <umangsharma1194@gmail.com>
2025-12-23 16:49:05 +0530

    changes made on same file in origin and master both

--- src/test/java/com/crm/gitbash/New_Class2.java
+++ src/test/java/com/crm/gitbash/New_Class2.java
@@ -5,7 +5,6 @@ public class New_Class2
 	public static void main(String[] args)
 	{
 		System.out.println("Hello World hi");
-		System.out.println("I am fine thank you.");
 		System.out.println("how are you ?");
 	}
 }
